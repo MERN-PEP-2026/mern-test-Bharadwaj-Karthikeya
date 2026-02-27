82072e3f17a4b5910172d643e64294ad9fbd198e
Bharadwaj Karthikeya <pbk311205@gmail.com>
2026-02-27 14:57:29 +0530

    frontend api update

--- frontend/src/api/course.api.js
+++ frontend/src/api/course.api.js
@@ -1,5 +1,9 @@
 import API from "./axios";
 
 export const getCourses = () => API.get("/courses");
+export const getCourseDetails = (id) => API.get(`/courses/${id}`);
 export const createCourse = (data) => API.post("/courses", data);
-export const deleteCourse = (id) => API.delete(`/courses/${id}`);
+export const updateCourse = (id, data) =>
+  API.put(`/courses/${id}`, data);
+export const deleteCourse = (id) =>
+  API.delete(`/courses/${id}`);
