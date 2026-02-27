88c709ebd7cf7eccfb8ae48d0aedc49231e40fbf
Bharadwaj Karthikeya <pbk311205@gmail.com>
2026-02-27 22:51:58 +0530

    Final deployment and push complete

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,7 @@ app.use(
     origin: [
       "https://mern-test-bharadwaj-karthikeya.vercel.app",
       process.env.FRONTEND_URL,
-      "http://localhost:3000",
+      "http://localhost:5173",
     ],
     methods: ["GET", "POST", "PATCH", "DELETE" ,],
     credentials: true,
