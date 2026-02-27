0544a4bcca1570c45779973b79a9053e8701d075
Bharadwaj Karthikeya <pbk311205@gmail.com>
2026-02-27 21:54:35 +0530

    cors fix

--- backend/app.js
+++ backend/app.js
@@ -11,17 +11,7 @@ const app = express();
 
 connectDB();
 
-app.use(
-  cors({
-    origin: [
-      "https://mern-test-bharadwaj-karthikeya.vercel.app/",
-      process.env.FRONTEND_URL,
-      "http://localhost:3000",
-    ],
-    methods: ["GET", "POST", "PATCH", "DELETE", "PUT"],
-    credentials: true,
-  }),
-);
+app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 app.use("/api/auth", authRoutes);
