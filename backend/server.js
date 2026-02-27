bcdf613b8caa05f183884db165b0bb46cbaee277
Bharadwaj Karthikeya <pbk311205@gmail.com>
2026-02-27 10:05:57 +0530

    sever.js created

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+
+const app = express();
+
+app.get('/api', (req, res) => {
+  res.json({ message: 'Hello from the backend!' });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
